8ba8e93aad60341eb7c472d0deb82e47e84a49d3
Nyanyi <yprentis@gmail.com>
2020-05-15 17:23:21 +0200

    adding more parsers

--- server3.py
+++ server3.py
@@ -1,6 +1,5 @@
 import http.server as SimpleHTTPServer
 import socketserver as SocketServer
-import logging
 import sys
 from  urllib import parse
 
@@ -9,8 +8,27 @@ port = int(sys.argv[1])
 class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
 		parsed_path = parse.urlparse(self.path)
-		query = parsed_path.query
-		print(query)
+		message_parts = [
+            	'CLIENT VALUES:',
+            	'>client_address={} ({})'.format(
+                self.client_address,
+                self.address_string()),
+            	'>command={}'.format(self.command),
+            	'>path={}'.format(self.path),
+            	'>real path={}'.format(parsed_path.path),
+            	'>query={}'.format(parsed_path.query),
+            	'>request_version={}'.format(self.request_version),
+		'',
+           	'HEADERS RECEIVED:',
+        	]
+		for name, value in sorted(self.headers.items()):
+			message_parts.append(
+				'>{}={}'.format(name, value.rstrip())
+			)
+		message_parts.append('')
+		message = '\r\n'.join(message_parts)
+		print(message)
+
 		SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
 
